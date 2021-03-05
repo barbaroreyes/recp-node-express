8282ad2dcc4f18d7d0c36ef10c1d25a1baa14f04
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2021-03-04 19:29:14 -0600

    adedd-tools

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const express = require('express');
+const shortid = require('shortid')
 const app = express();
 const PORT = 5000;
-
-app.get('/', (req,res)=>{
-    res.json({Hello : 'World'})
-});
+app.use(express.json())
 
 
-app.get('/hello',(req,res)=>{
-  res.json({hello : "Future sinior softwere developer"})
+const channels = [];
+const lessons = [];
+
+app.get('/',(req,res)=> {
+   res.json({data : 'Hello'})
 })
 
+app.post('/api/lessons',(req,res) => {
+  const lessonsInfo = req.body;
+  lessonsInfo.id = shortid.generate();
+  lessons.push(lessonsInfo);
+  res.status(201).json({lessonsInfo})
+})
+app.get('/api/lessons',(req,res)=>{
+    res.status(201).json(lessons)
+})
+app.post('/api/channels' ,(req,res) => {
+   const channelInfo = req.body;
+   channelInfo.id = shortid.generate();
+   channels.push(channelInfo)
+   res.status(201).json({channelInfo})
+})
+app.get('/api/channels',(req,res)=>{
+    res.status(201).json({channels});
+})
+
+app.delete('/api/channels/:id',(req,res) => {
+    const {id} = req.params;
+    const  deleted = channels.find(channel => channel.id === id);
+    res.status(200).json(deleted)
+    if(deleted){
+        channels = channels.filter(channel => channel.id !== id)
+    }else{
+        res
+        .status(404)
+        .json({message : "Channel you are looking for does not exit"})
+    }
+    
+});
 
 app.listen(PORT,() => console.log(`Running on Port ${PORT}`))
